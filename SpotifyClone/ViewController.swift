3678f9a8182097b7bdc1417dc07225f5a88ae3f2
Silas S. Caxias <silas_caxias@crgl-thirdparty.com>
2021-08-12 20:36:05 -0300

    Initial Commit

---
+++ SpotifyClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotifyClone
+//
+//  Created by Silas Da Silva Caxias on 12/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
